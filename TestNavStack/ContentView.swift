4fb076bc3d6388f4cd7fd870a72d2a7b911c784b
Tim Yoon <stimyoon@gmail.com>
2022-12-09 15:52:53 -0500

    First

--- TestNavStack/ContentView.swift
+++ TestNavStack/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+struct Person: Identifiable, Hashable {
+    var id: String? = UUID().uuidString
+    var name = ""
+    var imageName = ""
+    var logs: [String] = []
+}
 
-struct ContentView: View {
+enum PersonViewType: CaseIterable {
+    case string, image
+}
+class PersonListVM: ObservableObject {
+    @Published var persons: [Person] = []
+    init(){
+        persons.append(Person(name: "John", imageName: "person", logs: ["hellow", "goodbey"]))
+        persons.append(Person(name: "Mary", imageName: "photo", logs: ["Christopher", "🥹"]))
+    }
+}
+struct PersonImageView: View {
+    let person: Person
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: person.imageName)
+            List {
+                ForEach(person.logs, id: \.self) { log in
+                    NavigationLink {
+                        LogStringView(person: person, log: log)
+                    } label: {
+                        Text("\(log)")
+                    }
+                    
+                    
+                }
+            }
         }
-        .padding()
+    }
+    
+}
+
+struct LogStringView: View{
+    let person: Person
+    let log: String
+    
+    var body: some View{
+        Text(log)
+    }
+}
+
+struct PersonStringView: View {
+    let person: Person
+    var body: some View {
+        Text("Name: \(person.name)")
+    }
+}
+struct ContentView: View {
+    @StateObject var vm = PersonListVM()
+    @State private var selectedPerson: Person?
+    
+    var body: some View {
+        NavigationStack{
+            List {
+                ForEach(vm.persons) { person in
+                    NavigationLink(value: person) {
+                        Text("\(person.name)")
+                    }
+                }
+            }
+            .navigationDestination(for: Person.self){ person in
+                PersonImageView(person: person)
+            }
+        }
     }
 }
 

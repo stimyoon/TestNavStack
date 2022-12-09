d96a3b1af1cf8f67786d22ed596afec86578cb64
Tim Yoon <stimyoon@gmail.com>
2022-12-09 15:23:58 -0500

    Initial Commit

---
+++ TestNavStack/TestNavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNavStackApp.swift
+//  TestNavStack
+//
+//  Created by Tim Yoon on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TestNavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
